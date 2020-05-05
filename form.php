3f4208fd9f9ff3f038daadb3ed93805edcca6deb
LucianoCaturano <luciano.caturano@gmail.com>
2020-05-05 12:26:09 +0200

    progression formulaire

---
+++ form.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Todo List</title>
+    </head>
+    <body>
+     <h2>Ajouter une tâche</h2>
+        <h3>La tâche à effectuer</h3>
+             <form action="cible.php" method="post">
+                 <p>
+                     <textarea name="message">Nouvelle tâche</textarea>
+                     <input type="submit" value="Ajouter" />
+                 </p>
+             </form>
+    </body>
+</html>
